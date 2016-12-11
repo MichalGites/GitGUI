8d0e697f37e1c1d2e363aaa7e8ef5bb6699dcb64
Michal Berezanski <michal.berezanski@op.pl>
2016-12-11 21:30:22 +0100

    Revert "0.0.2"
    This reverts commit 347804bcfe76b73150b7575d9fbaeafc20ba90ee.

--- src/Testowy/Main.java
+++ src/Testowy/Main.java
@@ -4,11 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Siema gitowcy!");
-		System.out.println("Za³apa³o");
-		String ver = "VER ";
-		// wersja aplikacji nr 2
 
 	}
 
